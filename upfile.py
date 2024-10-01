a477ee0183602489f5e898ebe64fed5947c1507c
athulraj2006 <athulraj2028@cs.sjcetpalai.ac.in>
2024-10-01 15:06:11 +0530

    Add files via upload

---
+++ upfile.py
@@ -0,0 +1,10 @@
+name=input("enter your name")
+print(name)
+name1=input("enter your place")
+print(name1)
+roll_number=int(input("enter your roll number"))
+roll_number=roll_number+1
+print(roll_number)
+cgpa=float(input("enter your cgpa"))
+percent=cgpa*10
+print(percent)
