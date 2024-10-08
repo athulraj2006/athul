51a8206a1e4e227f2d39bc92e9e899239c46e248
athulraj2006 <athulraj2028@cs.sjcetpalai.ac.in>
2024-10-08 15:06:00 +0530

    Add files via upload

---
+++ python pr2 task1.py
@@ -0,0 +1,17 @@
+#created by athul raj
+#date:8-10-2024
+num1=int(input("Enter first number:"))
+num2=int(input("Enter second number:"))
+num3=int(input("Enter third number:"))
+sum=num1+num2
+print("The sum of num1 and num2 is:",sum)
+sub=num1-num2
+print("The difference when num2 is subtracted by num1 is:",sub)
+product=num1+num2
+print("The product of num1 and num2 is:",product)
+quotient=num1/num2
+print("The quotient when num1 is divided by num 2 is:",quotient)
+modulus=num1%num2
+print("The remainder when num1 is divided by num2 is:",modulus)
+combined=(num1+num2)*num3/2
+print("The result of (num1+num2)*num3/2 is:",combined)
