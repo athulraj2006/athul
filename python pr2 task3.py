7b83f350034be2ec7babdd3f5255c808e8ac181e
athulraj2006 <athulraj2028@cs.sjcetpalai.ac.in>
2024-10-08 16:20:24 +0530

    Add files via upload
    Title: Familiarize time and date in various formats (Eg. “Thu Jul 11 10:26:23 IST 2024”).

---
+++ python pr2 task3.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+print(datetime.now())
+current_time=datetime.now()
+format1=current_time.strftime("%Y-%m-%d %h:%m:%s")
+print(format1)
+format2=current_time.strftime("%d-%B-%y ")
+print(format2)
+
+
