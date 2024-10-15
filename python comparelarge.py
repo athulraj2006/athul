a68e2d0b3f51377a011cde902149051d2956d433
athulraj2006 <athulraj2028@cs.sjcetpalai.ac.in>
2024-10-15 14:09:10 +0530

    Add files via upload

---
+++ python comparelarge.py
@@ -0,0 +1,9 @@
+#created by athul raj
+#date:15-10-24
+num=int(input("enter a number"))
+if num>0:
+    print("the number is positive")
+elif num<0:
+    print("the number is negative")
+else:
+    print("the number is zero")
