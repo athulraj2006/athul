2721743fee2aa3da76d7ca2d4ba9a55eab461a5a
athulraj2006 <athulraj2028@cs.sjcetpalai.ac.in>
2024-10-15 15:39:57 +0530

    Add files via upload
    ticket

---
+++ ques ticket.py
@@ -0,0 +1,9 @@
+#created by athul raj
+#date:15-8-24
+age=int(input("enter an age:"))
+if age<10:
+    print("your fair is 7")
+elif age>=10:
+    print("ticket fair is 10")
+else:
+    print("the ticket fair is 5")
