ee53ed6bcdb6a7da2fa6e964c8e1d88af1c98119
athulraj2006 <athulraj2028@cs.sjcetpalai.ac.in>
2024-11-30 10:11:05 +0530

    Update lengths of three sides of a triangle.py

--- lengths of three sides of a triangle.py
+++ lengths of three sides of a triangle.py
@@ -4,9 +4,9 @@ def righttrianglecheck():
     a=int(input("enter first side:"))
     b=int(input("enter second side:"))
     c=int(input("enter third side:"))
-    list=[a,b,c]
-    list.sort()
-    if list[2]**2==list[0]**2+list[1]**2:
+    list1=[a,b,c]
+    list1.sort()
+    if list1[2]**2==list1[0]**2+list1[1]**2:
         print("It is a rirht triangle")
     else:
         print("It is not a right triangle")
